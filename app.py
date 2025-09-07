32af3c730a1181bf7e2f389110811a3608d400b1
Alejandro Garcia <agarciaso02@gmail.com>
2025-09-07 19:21:30 +0200

    Update app for production deployment

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # connect4_game/app.py
+import os
 from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
@@ -146,4 +147,4 @@ def reset_score():
     return jsonify(get_full_state())
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
